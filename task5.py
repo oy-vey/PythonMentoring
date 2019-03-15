6a73f7fabad50f4ed6a1ae8eaeba339d5276cc45
John Cooper <sdohla@tut.by>
2019-03-15 15:21:06 +0300

    Wrong commit by task5

--- task5.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-
-def get_links(html):
-    url_list = []
-    soup = BeautifulSoup(html, 'html.parser')
-    for link in soup.find_all('a'):
-        url_list.append(link.get('href'))
-    return url_list
